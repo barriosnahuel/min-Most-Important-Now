df021fadc4de8d48e6f302d43f81baeaa92f59f9
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-06-28 22:40:46 -0300

    Change Google Plus number of results from 15 to 10.

--- js/app/service/socialNetworks.js
+++ js/app/service/socialNetworks.js
@@ -138,7 +138,7 @@ app.service.socialNetworks.gplus = (function () {
     var findNews = function (keyword, onSuccess) {
         var apiKey = 'AIzaSyCNQ1slAxWLz8pg6MCPXJDVdeozgQBYxz8';
 
-        var url = 'https://www.googleapis.com/plus/v1/activities?key=' + apiKey + '&query=' + keyword + '&maxResults=15&orderBy=best&language=' + navigator.language;
+        var url = 'https://www.googleapis.com/plus/v1/activities?key=' + apiKey + '&query=' + keyword + '&maxResults=10&orderBy=best&language=' + navigator.language;
 
         $.getJSON(url, onSuccess);
     };
